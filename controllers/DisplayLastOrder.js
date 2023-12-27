f9fef09d36d767903000e078d396c39498ad1a9b
roleigh1 <robin.leigh@outlook.de>
2023-12-28 00:41:47 +0100

    send the data from last order to the db and also created a object with it

--- controllers/DisplayLastOrder.js
+++ controllers/DisplayLastOrder.js
@@ -1,21 +1,35 @@
 const { get } = require('http');
-const {Orders} = require('../models/models');
+const { Orders } = require('../models/models');
 
-const getlastOrder = async (req,res) => {
-   try{
-    const lastRecord = await Orders.findOne({
-        order:[['id','DESC']],
-    });
+const getlastOrder = async (req, res) => {
+    try {
+        const lastRecord = await Orders.findOne({
+            order: [['id', 'DESC']],
+        });
 
-    if(lastRecord) {
-        console.log('The Last Order', lastRecord.toJSON()); 
+        if (lastRecord) {
+          
+            lastRecord.toJSON();
+            const lastOrderDetails = {
+                id: lastRecord.id,
+                email: lastRecord.email,
+                items: lastRecord.item,
+                totalPrice: lastRecord.total,
+                pickupdate: lastRecord.pickupdate,
+                created: lastRecord.createdAt
+            }
+            console.log(lastOrderDetails); 
+            res.json(lastOrderDetails)
+        } else {
+            console.log("Table is empty");
 
-    } else {
-        console.log("Table is empty"); 
+        }
+    } catch (error) {
+        console.error('Error:', error);
+   
     }
-} catch (error) {
-    console.error('Fehler:', error);
-} 
- } 
- getlastOrder(); 
-module.exports = getlastOrder
+}
+ 
+
+
+module.exports = {getlastOrder}; 
--- routes/authroute.js
+++ routes/authroute.js
@@ -3,11 +3,11 @@ const  logintest  = require('../controllers/authController');
 const counterDB = require('../controllers/countOrders');
 const passport = require('passport');
 const router = express.Router();
-const lastOrderController = require('../controllers/DisplayLastOrder')
+const displayLastOrder = require('../controllers/DisplayLastOrder')
 router.get("/counterDB",passport.authenticate("jwt", { session: false }), counterDB.countOrders) ; 
 router.get("/counterMonday",passport.authenticate("jwt" , {session: false}), counterDB.countCreatedAt);
 router.post('/login',  logintest.login);
-router.get()
-console.log(lastOrderController); 
+router.get("/lastOrder",displayLastOrder.getlastOrder)
+
 module.exports = router;
 
