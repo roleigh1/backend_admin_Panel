9e6917ebd8c6dfc53eaf570278244394269c1899
roleigh1 <robin.leigh@outlook.de>
2024-03-08 21:01:06 +0100

    finished the implementation of the pagination at getallorders

--- controllers/orders.js
+++ controllers/orders.js
@@ -3,11 +3,17 @@ const { Orders, FinishedOrders } = require("../models/models");
 
 const getAllOrders = async (req, res) => {
     try {
-        const allOrders = await Orders.findAll({});
+        const page = parseInt(req.query.page) || 1; 
+        const pageSize = parseInt(req.query.pageSize) || 10; 
+        const offset = ( page - 1 ) * pageSize; 
+        const allOrders = await Orders.findAll({
+            offset,
+            limit:pageSize,
+        });
         res.status(200).json({ allOrders })
     } catch (error) {
         console.error("Error getting all orders", error);
-        res.status(400).json({ message: "Error sending Data" });
+        res.status(400).json({ message: "Error sending Data", error: error.message });
     }
 }
 const finishOrder = async (req, res) => {
