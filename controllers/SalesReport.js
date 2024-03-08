5a0662dd1f4f5f6d8eb7700f32bb2c629aef692e
roleigh1 <robin.leigh@outlook.de>
2024-03-08 22:20:43 +0100

    shortened the salesreport and reduced the endpoints in the route

--- controllers/SalesReport.js
+++ controllers/SalesReport.js
@@ -1,274 +1,61 @@
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 const { Orders } = require('../models/models');
-const getTotalPrice = async (req,res) => {
-    const startDate = new Date('2023-01-01');
-    const endDate = new Date('2222-01-31');
-    try {
-        const sumTotal = await Orders.findAll({
-            attributes: ['total'],
-            where: {
-                createdAt: {
-                    [Op.between]: [startDate, endDate]
-                },
-            },
-        })
-        const totalSum = sumTotal.reduce((a, b) => {
-            return a + b.total;
-        }, 0)
-        res.json({totalSum}); 
- 
-    } catch (error) {
-        console.error('Error: ', error);
-        res.status(500).json({ error: 'An error occurred' });
-    }
-}
-const getTotalJan = async (req,res) => {
-    const startDate = new Date('2023-01-01');
-    const endDate = new Date('2023-01-31');
-    try {
-        const sumJan = await Orders.findAll({
-            attributes: ['total'],
-            where: {
-                createdAt: {
-                    [Op.between]: [startDate, endDate]
-                },
-            },
-        })
-        const jan = sumJan.reduce((a, b) => {
-            return a + b.total;
-        }, 0)
-        res.json({jan}); 
-    } catch (error) {
-        console.error('Error: ', error);
-    }
-}
-const getTotalFeb = async (req,res) => {
-    const startDate = new Date('2023-02-01');
-    const endDate = new Date('2023-02-28');
-    try {
-        const sumFeb = await Orders.findAll({
-            attributes: ['total'],
-            where: {
-                createdAt: {
-                    [Op.between]: [startDate, endDate]
-                },
-            },
-        })
-        const feb = sumFeb.reduce((a, b) => {
-            return a + b.total;
-        }, 0)
-        res.json({feb})
-    } catch (error) {
-        console.error('Error: ', error);
-    }
-}
-const getTotalMar = async (req,res) => {
-    const startDate = new Date('2023-01-01');
-    const endDate = new Date('2023-03-31');
-    try {
-        const sumMar = await Orders.findAll({
-            attributes: ['total'],
-            where: {
-                createdAt: {
-                    [Op.between]: [startDate, endDate]
-                },
-            },
-        })
 
-        const mar = sumMar.reduce((a, b) => {
-            return a + b.total;
-        }, 0)
-        res.json({mar})
-    } catch (error) {
-        console.error('Error: ', error);
-    }
-}
-const getTotalApr = async (req,res) => {
-    const startDate = new Date('2023-04-01');
-    const endDate = new Date('2023-04-30');
-    try {
-        const sumApr = await Orders.findAll({
-            attributes: ['total'],
-            where: {
-                createdAt: {
-                    [Op.between]: [startDate, endDate]
-                },
-            },
-        })
-        const apr = sumApr.reduce((a, b) => {
-            return a + b.total;
-        }, 0)
-        res.json({apr});
-    } catch (error) {
-        console.error('Error: ', error);
-    }
-}
-const getTotalMay = async (req,res) => {
-    const startDate = new Date('2023-05-01');
-    const endDate = new Date('2023-05-31');
-    try {
-        const sumMay = await Orders.findAll({
-            attributes: ['total'],
-            where: {
-                createdAt: {
-                    [Op.between]: [startDate, endDate]
-                },
-            },
-        })
-        const may = sumMay.reduce((a, b) => {
-            return a + b.total;
-        }, 0)
-        res.json({may});
-    } catch (error) {
-        console.error('Error: ', error);
-    }
-}
-const getTotalJune = async (req,res) => {
-    const startDate = new Date('2023-06-01');
-    const endDate = new Date('2023-06-30');
-    try {
-        const sumJun = await Orders.findAll({
-            attributes: ['total'],
-            where: {
-                createdAt: {
-                    [Op.between]: [startDate, endDate]
-                },
-            },
-        })
-        const jun = sumJun.reduce((a, b) => {
-            return a + b.total;
-        }, 0)
-        res.json({jun})
-    } catch (error) {
-        console.error('Error: ', error);
-    }
-}
-const getTotalJuly = async (req,res) => {
-    const startDate = new Date('2023-07-01');
-    const endDate = new Date('2023-07-31');
-    try {
-        const sumJul = await Orders.findAll({
-            attributes: ['total'],
-            where: {
-                createdAt: {
-                    [Op.between]: [startDate, endDate]
-                },
-            },
-        })
-       const jul = sumJul.reduce((a, b) => {
-            return a + b.total;
-        }, 0)
-        res.json({jul}); 
-    } catch (error) {
-        console.error('Error: ', error);
-    }
-}
-const getTotalAug = async (req,res) => {
-    const startDate = new Date('2023-08-01');
-    const endDate = new Date('2023-08-31');
-    try {
-        const sumAug = await Orders.findAll({
-            attributes: ['total'],
-            where: {
-                createdAt: {
-                    [Op.between]: [startDate, endDate]
-                },
-            },
-        })
-        const aug = sumAug.reduce((a, b) => {
-            return a + b.total;
-        }, 0)
-        res.json({aug})
-    } catch (error) {
-        console.error('Error: ', error);
-    }
-}
-const getTotalSep = async (req,res) => {
-    const startDate = new Date('2023-09-01');
-    const endDate = new Date('2023-09-30');
-    try {
-        const sumSep = await Orders.findAll({
-            attributes: ['total'],
-            where: {
-                createdAt: {
-                    [Op.between]: [startDate, endDate]
-                },
-            },
-        })
 
-        const sep = sumSep.reduce((a, b) => {
-            return a + b.total;
-        }, 0)
-        res.json({sep})
-    } catch (error) {
-        console.error('Error: ', error);
-    }
-}
-const getTotalOct = async (req,res) => {
-    const startDate = new Date('2023-10-01');
-    const endDate = new Date('2023-10-31');
-    try {
-        const sumOct = await Orders.findAll({
-            attributes: ['total'],
-            where: {
-                createdAt: {
-                    [Op.between]: [startDate, endDate]
-                },
-            },
-        })
-       const oct = sumOct.reduce((a, b) => {
-            return a + b.total;
-        }, 0)
-        res.json({oct}); 
-    } catch (error) {
-        console.error('Error: ', error);
-    }
-}
-const getTotalNov = async (req,res) => {
-    const startDate = new Date('2023-11-01');
-    const endDate = new Date('2023-11-30');
-    try {
-        const sumNov = await Orders.findAll({       
-            attributes: ['total'],
-            where: {
-                createdAt: {
-                    [Op.between]: [startDate, endDate]
-                },
-            },
-        })
-        const nov = sumNov.reduce((a, b) => {
-            return a + b.total;
-        }, 0)
-        res.json({nov})
-    } catch (error) {
-        console.error('Error: ', error);
-    }
-}
-const getTotalDec = async (req,res) => {
-    const startDate = new Date('2023-12-01');
-    const endDate = new Date('2023-12-31');
-    try {
-        const sumDec = await Orders.findAll({
-            attributes: ['total'],
-            where: {
-                createdAt: {
-                    [Op.between]: [startDate, endDate]
-                },
-            },
-        })
 
-        const dec = sumDec.reduce((a, b) => {
-            return a + b.total;
-        }, 0)
-        res.json({dec})
+
+
+
+const getTotalMonth = async (req, res) => {
+    try {
+      const month = req.params.month.toLowerCase();
+      const { startDate, endDate } = getMonthDateRange(month);
+  
+      const sumResult = await Orders.findAll({
+        attributes: ['total'],
+        where: {
+          createdAt: {
+            [Op.between]: [startDate, endDate],
+          },
+        },
+      });
+  
+      const monthTotal = sumResult.reduce((a, b) => a + b.total, 0);
+  
+      res.json({ [month]: monthTotal });
     } catch (error) {
-        console.error('Error: ', error);
+      console.error(`Error fetching ${month}`, error);
+      res.status(500).json({ error: `Error fetching ${month}` });
     }
-}
+  };
+  
+  const getMonthDateRange = (month) => {
+    const year = 2023; // 
+    const monthMap = {
+      jan: 1,
+      feb: 2,
+      mar: 3,
+      apr: 4,
+      may: 5,
+      jun: 6,
+      jul: 7,
+      aug: 8,
+      sep: 9,
+      oct: 10,
+      nov: 11,
+      dec: 12,
+    };
+  
+    const startOfMonth = new Date(year, monthMap[month] - 1, 1);
+    const endOfMonth = new Date(year, monthMap[month], 0);
+  
+    return {
+      startDate: startOfMonth,
+      endDate: endOfMonth,
+    };
+  };
 
 module.exports = {
-    getTotalPrice, getTotalJan, getTotalApr, getTotalAug,
-    getTotalDec, getTotalFeb, getTotalJuly, getTotalJune,
-    getTotalMar, getTotalMay, getTotalNov, getTotalSep, getTotalOct
+    getTotalMonth
 }
