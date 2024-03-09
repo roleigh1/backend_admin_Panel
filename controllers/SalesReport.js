d938c9e49950e0cd0433b3013313d0a1c5f562fe
roleigh1 <robin.leigh@outlook.de>
2024-03-09 01:04:32 +0100

    shorten the sales report and shorten it to one enpoint where all months and the total sum gonna get fetched

--- controllers/SalesReport.js
+++ controllers/SalesReport.js
@@ -2,59 +2,65 @@ const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 const { Orders } = require('../models/models');
 
-
-
-
-
-
 const getTotalMonth = async (req, res) => {
     try {
-      const month = req.params.month.toLowerCase();
-      const { startDate, endDate } = getMonthDateRange(month);
-  
-      const sumResult = await Orders.findAll({
-        attributes: ['total'],
-        where: {
-          createdAt: {
-            [Op.between]: [startDate, endDate],
-          },
-        },
-      });
-  
-      const monthTotal = sumResult.reduce((a, b) => a + b.total, 0);
-  
-      res.json({ [month]: monthTotal });
-    } catch (error) {
-      console.error(`Error fetching ${month}`, error);
-      res.status(500).json({ error: `Error fetching ${month}` });
-    }
+        const month = req.params.month.toLowerCase();
+        console.log(month)
+        if (month === "total") {
+            const totalYear =  await Orders.findAll({
+                attributes:["total"]
+            })
+            const totalSum = totalYear.reduce((a,b) => a + b.total,0); 
+            res.json({totalSum}); 
+        } else {
+            const { startDate, endDate } = getMonthDateRange(month);
+
+            const sumResult = await Orders.findAll({
+                attributes: ['total'],
+                where: {
+                    createdAt: {
+                        [Op.between]: [startDate, endDate],
+                    },
+                },
+            });
+
+            const monthTotal = sumResult.reduce((a, b) => a + b.total, 0);
+
+            res.json({ [month]: monthTotal });
+        }
+        } catch (error) {
+            console.error(`Error fetching ${month}`, error);
+            res.status(500).json({ error: `Error fetching ${month}` });
+        }
+    
+     
   };
-  
-  const getMonthDateRange = (month) => {
-    const year = 2023; // 
+
+const getMonthDateRange = (month) => {
+    const year = 2023; 
     const monthMap = {
-      jan: 1,
-      feb: 2,
-      mar: 3,
-      apr: 4,
-      may: 5,
-      jun: 6,
-      jul: 7,
-      aug: 8,
-      sep: 9,
-      oct: 10,
-      nov: 11,
-      dec: 12,
+        jan: 1,
+        feb: 2,
+        mar: 3,
+        apr: 4,
+        may: 5,
+        jun: 6,
+        jul: 7,
+        aug: 8,
+        sep: 9,
+        oct: 10,
+        nov: 11,
+        dec: 12,
     };
-  
+
     const startOfMonth = new Date(year, monthMap[month] - 1, 1);
     const endOfMonth = new Date(year, monthMap[month], 0);
-  
+
     return {
-      startDate: startOfMonth,
-      endDate: endOfMonth,
+        startDate: startOfMonth,
+        endDate: endOfMonth,
     };
-  };
+};
 
 module.exports = {
     getTotalMonth
